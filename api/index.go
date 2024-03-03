0cdc131fd1d45ab16fedef8e67335afe4b6e98d2
Thomas Crombez <me@thomascrmbz.com>
2024-03-03 13:58:12 +0100

    Added twirp server

--- api/index.go
+++ api/index.go
@@ -1,12 +1,45 @@
 package handler
 
 import (
+	"context"
+	"fmt"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/thomascrmbz/vercel-go/onboarding"
+	"github.com/twitchtv/twirp"
 )
 
 var mux = chi.NewRouter()
+
+type Service struct{}
+
+func (*Service) SetupLocation(context.Context, *onboarding.SetupLocationRequest) (*onboarding.SetupLocationResponse, error) {
+	return &onboarding.SetupLocationResponse{}, nil
+}
+
+func (*Service) InstallerSetupLocation(context.Context, *onboarding.InstallerSetupLocationRequest) (*onboarding.InstallerSetupLocationResponse, error) {
+	return &onboarding.InstallerSetupLocationResponse{}, nil
+}
+func (*Service) ClaimLocation(context.Context, *onboarding.ClaimLocationRequest) (*onboarding.ClaimLocationResponse, error) {
+	return &onboarding.ClaimLocationResponse{}, nil
+}
+
+func (*Service) SendClaimMail(context.Context, *onboarding.SendClaimMailRequest) (*onboarding.SendClaimMailResponse, error) {\
+	return &onboarding.SendClaimMailResponse{}, nil
+}
+
+func (*Service) GetClaimCode(ctx context.Context, req *onboarding.GetClaimCodeRequest) (*onboarding.GetClaimCodeResponse, error) {
+	fmt.Println(req.Code, req.Sn)
+
+	return &onboarding.GetClaimCodeResponse{
+		ClaimCode: "123456",
+		LocationId: "123",
+		Claimed: true,
+		ClaimedBy: nil,
+		GeneratedBy: "me",
+	}, nil
+}
 
 func init() {
 	mux.Get("/", func(w http.ResponseWriter, r *http.Request) {
@@ -15,6 +48,12 @@ func init() {
 	mux.Get("/ping", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("pong"))
 	})
+
+	svc := &Service{}
+	ts := onboarding.NewOnboardingServer(svc)
+	mux.Use(ts)
+
+	fmt.Println(mux.Routes())
 }
 
 func Handler(w http.ResponseWriter, r *http.Request) {
--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module github.com/thomascrmbz/vercel-go
 
 go 1.22.0
 
-require github.com/go-chi/chi/v5 v5.0.12 // indirect
+require (
+	github.com/go-chi/chi/v5 v5.0.12
+	github.com/twitchtv/twirp v8.1.3+incompatible
+	google.golang.org/protobuf v1.32.0
+)
+
+require github.com/pkg/errors v0.9.1 // indirect
