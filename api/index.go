5cfe16454594fc128a58cd1dfd59498d6129c5a9
Thomas Crombez <me@thomascrmbz.com>
2024-03-03 15:40:02 +0100

    Added hello world message

--- api/index.go
+++ api/index.go
@@ -99,7 +99,7 @@ func init() {
 
 <body>
 <div>
-    <h1>Example Domain</h1>
+    <h1>👋 Hello World!</h1>
     <p>This domain is for use in illustrative examples in documents. You may use this
     domain in literature without prior coordination or asking for permission.</p>
     <p><a href="https://www.iana.org/domains/example">More information...</a></p>
